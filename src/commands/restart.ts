3f4be8b58a418b50b08b51cb406e5113d29a52bf
Aleksandr Minorin <koropa05@gmail.com>
2024-08-23 09:03:44 +0300

    fix

--- src/commands/restart.ts
+++ src/commands/restart.ts
@@ -3,9 +3,8 @@ import {Context} from "telegraf";
 
 export default async function(ctx:Context){
     if(ctx.chat?.id === 6018898378){
-        await ctx.reply('Бот выключен!')
-        setTimeout(() => {
+        await ctx.reply('Бот выключен!').then(()=>{
             process.exit(0)
-        }, 1000 * 60)
+        })
     }else{await ctx.reply('Написано же ДЛЯ АДМИНОВ')}
 }
