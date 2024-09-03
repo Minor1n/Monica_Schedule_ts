af9d5d21eeb5a6b685b0f15d9349facb9d1899ac
Aleksandr Minorin <koropa05@gmail.com>
2024-09-03 18:47:10 +0300

    fix

--- src/handlers/CronHandler.ts
+++ src/handlers/CronHandler.ts
@@ -1,6 +1,7 @@
 import {CronJob} from "cron";
 import commands from "../commands"
 import payments from "../payments";
+import tables from "../tables";
 
 const createCronJob = (cronTime: string, onTick: () => Promise<void>) => {
     new CronJob(cronTime, async () => {
@@ -13,8 +14,12 @@ const createCronJob = (cronTime: string, onTick: () => Promise<void>) => {
 };
 
 export default () => {
-    createCronJob('0 10 14 * * 1-5,7', () => commands.fetch(6018898378));
+    createCronJob('0 0 10 * * 1-5,7', () => commands.fetch(6018898378));
+    createCronJob('0 0 12 * * 1-5,7', () => commands.fetch(6018898378));
+    createCronJob('0 0 14 * * 1-5,7', () => commands.fetch(6018898378));
+    createCronJob('0 0 16 * * 1-5,7', () => commands.fetch(6018898378));
+    createCronJob('0 0 18 * * 1-5,7', () => commands.fetch(6018898378));
     createCronJob('0 0 13 1 * *', payments.recount);
     createCronJob('0 35 7 25-31 * *', payments.preAlert);
-    //createCronJob('0 10 14 * * 6', Functions.duty.sender);
+    createCronJob('0 10 14 * * 6', tables.duty);
 };
