59dfcf1fdf3d87d67942c7be4ff5beb87aa00182
Aleksandr Minorin <koropa05@gmail.com>
2024-09-08 17:41:10 +0300

    fix

--- src/handlers/CronHandler.ts
+++ src/handlers/CronHandler.ts
@@ -21,5 +21,5 @@ export default () => {
     createCronJob('0 0 18 * * *', () => commands.fetch(6018898378));
     createCronJob('0 0 13 1 * *', payments.recount);
     createCronJob('0 35 7 25-31 * *', payments.preAlert);
-    createCronJob('0 10 14 * * 7', tables.duty);
+    createCronJob('0 0 20 * * 6', tables.duty);
 };
